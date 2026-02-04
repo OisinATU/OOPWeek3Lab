98aafa184e706057e9aa2aeb5eb5f01ba38dda05
OisinATU <G00470518@atu.ie>
2026-02-04 19:29:00 +0000

    Created Hello Endpoint

--- src/main/java/ie/atu/java/oopweek3lab/RequestController.java
+++ src/main/java/ie/atu/java/oopweek3lab/RequestController.java
@@ -1,4 +1,19 @@
 package ie.atu.java.oopweek3lab;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class RequestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+
 }
